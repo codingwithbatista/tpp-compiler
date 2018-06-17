d777149300edbe53cfe43324ee6898f794d8777a
Paulo Batista <pauloc@linuxmail.org>
2018-06-17 01:58:55 -0300

    verificando coercao de tipos

--- semantic/Semantic.py
+++ semantic/Semantic.py
@@ -5,27 +5,12 @@ class semantic_module(object):
 
     def __init__(self, syntax_tree=Node):
         self.syntax_tree = syntax_tree
-        self.table = []
+        self.SymbolTable = []
         self.defineScopes()
-        self.defineDataTypes()
-        self.defineNumberDataType()
-        #self.printVariableDefinedTable()
-        self.verifyVariableNotDeclared()
-        self.verifyVariableUses()
-        self.verifyVariableAlreadyDefined()
-        self.createDeclaredFunctionsTable()
-        self.call_function_annotation()
-        self.defineExpressionDataType()
-        #self.printTree()
-        self.verifyTypeAssignment()
-        self.printFunctionsDefinedTable()
-        self.verifyReturnType()
-        self.verifyUnusedFunctions()
-        self.verifyNumberOfFunctionsParameters()
-        #self.scope_definition()
-        #self.declare_previous_verification()
-        #self.print_simbol_table()
-        #self.verify_semantic_errors()
+        self.walking()
+        self.printTree()
+        self.printTable(self.SymbolTable)
+        
 
     def printNode(self, node):
         if hasattr(node, 'lexeme'):
@@ -50,14 +35,10 @@ class semantic_module(object):
             self.printNode(node)
 
 
-    def printVariableDefinedTable(self):
-        for line in self.definedVariableTable:
-            print(line)
+    def printTable(self, table=[]):
+        for t in table:
+            print(t)
 
-
-    def printFunctionsDefinedTable(self):
-        for line in self.declaredFunctions:
-            print(line)
 
     def defineScopes(self):
         scope = ""
@@ -107,259 +88,61 @@ class semantic_module(object):
                 node.scope = scope
     
 
-    def createDefinedVariableTable(self):
-        table = []
-        for node in PreOrderIter(self.syntax_tree):
-            try:
-                if node.op_type == "declaration" or node.op_type == "fnc_parameter":
-                    table.append([node.number, node.scope, node.data_type, node.tokenval, node.lexeme, node.line, node.op_type])
-            except AttributeError:
-                pass
-
-        self.definedVariableTable = table
-
-    
-    def delegateDataTypeToVarList(self, data_type=str, varListNode=Node):
-        for node in PreOrderIter(varListNode):
-            node.data_type = data_type
-            node.op_type = "declaration"
+    def addVarDeclareSymbolTable(self, node=Node):
+        variables = []
+        for n in PreOrderIter(node):
+            if n.name == "TYPE":
+                data_type = n.lexeme
+            elif n.name == "ID":
+                variables.append(["var_declare",data_type,n.lexeme, n.scope, n.line])
+        for v in variables:
+            self.SymbolTable.append(v)
 
 
-    def delegateDataTypeToParameterFunction(self, data_type=str, varParameterNode=Node):
-        varParameterNode.data_type = data_type
-        varParameterNode.op_type = "fnc_parameter"
-    
-    def delegateDataTypeToDeclaredVariable(self, varNode):
-        for line in self.definedVariableTable:
-            if (line[1] in varNode.scope) and  (line[4] == varNode.lexeme):
-                varNode.data_type = line[2]
+    def annotateVarTypes(self, varNode=Node):
+        node = varNode.children[0]
+        for line in self.SymbolTable:
+            if line[0] == "var_declare":
+                if node.lexeme == line[2] and line[3] in node.scope:
+                    node.data_type = line[1]
 
 
-
-    def defineDataTypes(self):
-        for node in PreOrderIter(self.syntax_tree):
-            if node.name == "VAR_DECLARE" and node.children[2].children[0].children[0].name != "VAR_INDEX_STMT":
-                data_type = node.children[0].lexeme
-                self.delegateDataTypeToVarList(data_type, node.children[2])
-            elif node.name == "PARAMETER_STATEMENT_STMT":
-                data_type = node.children[0].lexeme
-                self.delegateDataTypeToParameterFunction(data_type, node.children[2])
-            #elif node.name == "VAR_INDEX_STMT":
-
-            #    data_type = node.parent.parent.parent.children[0]
-
-        self.createDefinedVariableTable()
-
-        for node in PreOrderIter(self.syntax_tree):
-            if node.name == "ID" and node.parent.parent.name != "VAR_LIST" and node.parent.name != "PARAMETER_STATEMENT_STMT":
-                self.delegateDataTypeToDeclaredVariable(node)
-
-    
-    def isDeclaredVariable(self, nodeVar):
-        if hasattr(nodeVar, 'data_type'):  
-            return True
-        return False
-
-    
-    def verifyVariableNotDeclared(self):
-        for node in PreOrderIter(self.syntax_tree):
-            if node.name == "VAR" and node.children[0].name != "VAR_INDEX_STMT":
-                
-                isDeclaredVar = self.isDeclaredVariable(node.children[0])
-                if isDeclaredVar == False:
-                    print("In line ", node.children[0].line,":\n", "variable '", node.children[0].lexeme,"' is not declared", sep="")
-
-
-    def isUsedVariable(self, line_variableDefinition):
-        for node in PreOrderIter(self.syntax_tree):
-            if node.name == "VAR" and node.parent.name != "VAR_LIST" and node.children[0].name != "VAR_INDEX_STMT":
-                varNode = node.children[0]
-                if line_variableDefinition[1] in varNode.scope and line_variableDefinition[4] == varNode.lexeme:
-                    return True
-        
-        return False
-
-
-    def verifyVariableUses(self):
-        for line in self.definedVariableTable:
-            isUsed = self.isUsedVariable(line)
-            if isUsed == False:
-                print("In line ", line[5],":\n", "the variable '", line[4],"' is declared and unused", sep="")
-
-
-    def verifyVariableAlreadyDefined(self):
-        size = len(self.definedVariableTable)
-        for i in range(size):
-            for j in range(i+1, size):
-                if (self.definedVariableTable[i][1] == self.definedVariableTable[j][1] and
-                self.definedVariableTable[i][4]  == self.definedVariableTable[j][4]):
-                    print("Variable '", self.definedVariableTable[i][4],"' is declared more than one time. See line ",
-                    self.definedVariableTable[i][5]," and ",self.definedVariableTable[j][5], sep="")
-    
-        
-    def defineNumberDataType(self):
-        for node in PreOrderIter(self.syntax_tree):
-            if node.name == "NUMBER":
-                if node.tokenval == "NUMERO_INTEIRO":
-                    node.data_type = "inteiro"
-                elif node.tokenval == "NUMERO_FLUTUANTE":
-                    node.data_type = "flutuante"
-    
-    
-    def defineExpressionDataType(self):
-        for node in PreOrderIter(self.syntax_tree):
-            if node.name == "TIMES_OPERATOR" or node.name == "SUM_OPERATOR":
-                node.data_type = self.verifyExpressionChildDataType(node)
-        
-        for node in PreOrderIter(self.syntax_tree):
-            if node.name == "EXPRESSION":
-                node.data_type = self.verifyExpressionChildDataType(node)
-    
-
-    def verifyExpressionChildDataType(self, op_node=Node):
-        for node in PreOrderIter(op_node):
-            if hasattr(node, "data_type"):
+    def annotateExpressionTypes(self, exprNode=Node):
+        for node in PreOrderIter(exprNode):
+            if hasattr(node, 'data_type'):
                 if node.data_type == "flutuante":
-                    return "flutuante"
+                    exprNode.data_type = "flutuante"
+                    break
                 elif node.data_type == "inteiro":
-                    data_type = "inteiro"
-        
-        data_type = "not_defined"
-        return data_type
-
-
-    def verifyTypeAssignment(self):
+                    exprNode.data_type = "inteiro"
+    
+    
+    def warningCastingTypes(self):
         for node in PreOrderIter(self.syntax_tree):
             if node.name == "ASSIGNMENT":
-                left = self.verifyExpressionChildDataType(node.children[0])
-                right = self.verifyExpressionChildDataType(node.children[1])
-                if (left == "inteiro" and right == "flutuante"):
-                    print("warning: casting to inteiro, in line ", node.line, ":\nvariable '", node.children[0].children[0].lexeme,
-                    "' is inteiro, but it receives flutuante data type.", sep="")
-                elif (left == "flutuante" and right == "inteiro"):
-                    print("warning: casting to flutuante, in line ", node.line, ":\nvariable '", node.children[0].children[0].lexeme,
-                    "' is flutuante, but it receives inteiro data type.", sep="")
-    
+                leftNode = node.children[0].children[0]
+                rightNode = node.children[1]
+                if leftNode.data_type != rightNode.data_type:
+                    print("===== WARNING =====\nIn line ", leftNode.line, " variable '", leftNode.lexeme,
+                    "is ", leftNode.data_type, ", but received ", rightNode.data_type)
 
-    def createDeclaredFunctionsTable(self):
-        table = []
+    def walking(self):
         for node in PreOrderIter(self.syntax_tree):
-            if node.name == "FUNCTION_DECLARATION":
-                numberOfParameters = self.getNumberOfFunctionsParameters(node)
-                if node.children[0].name == "TYPE":
-                    return_type = node.children[0].lexeme
-                else:
-                    return_type = "vazio"
-                try:
-                    if node.children[1].children[0].children[0].name == "ID":
-                        function_name = node.children[1].children[0].children[0].lexeme
-                        scope = node.children[1].children[0].children[0].scope
-                        table.append([function_name, scope, return_type, numberOfParameters])
-                except IndexError:
-                    if node.children[0].children[0].children[0].name == "ID":
-                        function_name = node.children[0].children[0].children[0].lexeme
-                        scope = node.children[0].children[0].children[0].scope
-                        table.append([function_name, scope, return_type, numberOfParameters])
-        self.declaredFunctions = table
-
-    def verifyReturnType(self):
-        for line in self.declaredFunctions:
-            hasReturn = self.functionHasReturnType(line)
-            if hasReturn[0] == False and line[2] != "vazio" and hasReturn[1] != "not_defined":
-                print("Semantic Error in function '", line[0], "':\nFunction returns vazio, but should returns ",
-                 line[2], sep="")
-                
-            elif hasReturn[0] == True and line[2] != hasReturn[1] and hasReturn[1] != "not_defined":
-                print("Semantic Error in function '", line[0],"':\n", 
-                "In line ", hasReturn[2], ", fuction should return ", line[2], " but it returns ",
-                hasReturn[1],  sep="")
-            
-
-    
-
-    def functionHasReturnType(self, functionDeclareLine=[]):
-        for node in PreOrderIter(self.syntax_tree):
-            if node.name == "RETURN_STMT" and node.scope == functionDeclareLine[1]:
-
-                return True, node.children[2].data_type, node.children[0].line
+            if node.name == "VAR_DECLARE":
+                self.addVarDeclareSymbolTable(node)
+            if node.tokenval == "NUMERO_INTEIRO":
+                node.data_type = "inteiro"
+            elif node.tokenval == "NUMERO_FLUTUANTE" :
+                node.data_type = "flutuante"
+            elif node.name == "VAR" and node.parent.name != "VAR_LIST":
+                self.annotateVarTypes(node)
         
-        return False, "vazio"
-    
-    def functionIsDeclared(self, node=Node):
-        if node.name == "CALL_FUNCTION_STMT":
-            func_name = node.children[0].children[0].lexeme
-            for line in self.declaredFunctions:
-                scope = "global.fnc_" + func_name
-                if scope == line[1]:
-                    return True, line[2], line[0], line[3]
+        for node in PostOrderIter(self.syntax_tree):
+            if node.name == "EXPRESSION":
+                self.annotateExpressionTypes(node)
+        self.warningCastingTypes()
+
         
-        return False, -1
-
-
-
-    def call_function_annotation(self):
-        for node in PreOrderIter(self.syntax_tree):
-            if node.name == "CALL_FUNCTION_STMT":
-                isDeclared = self.functionIsDeclared(node)
-                if isDeclared[0]:
-                    node.data_type = isDeclared[1]
-                else:
-                    func_node = node.children[0].children[0]
-                    print("In line ", func_node.line, ":\nThe function '", func_node.lexeme,"' is not declared"
-                    ,sep="")
-    
-
-    def isUsedFunction(self, lineDeclaredFuntion):
-        for node in PreOrderIter(self.syntax_tree):
-            if node.name == "CALL_FUNCTION_STMT":
-                funcName_node = node.children[0].children[0]
-                if funcName_node.lexeme == lineDeclaredFuntion[0]:
-                    return True
-        return False
-
-  
-    def verifyUnusedFunctions(self):
-        for line in self.declaredFunctions:
-            isUsed = self.isUsedFunction(line)
-            if isUsed == False and line[0] != "principal":
-                print("Warning:\nFunction '", line[0],"' was declared, but unused.", sep="")
-    
-
-    def getNumberOfFunctionsParameters(self, node=Node):
-        count = 0
-        for n in PreOrderIter(node):
-            if n.name == "PARAMETER_STATEMENT_STMT":
-                count += 1
-        
-        return count
-    
-
-    def getNumberOfCallParameters(self, callNode=Node):
-        parameters = []
-        for n in PreOrderIter(callNode):
-            if n.name == "EXPRESSION":
-                parameters.append(n.data_type)
-        
-        return len(parameters), parameters
-
-
-    def verifyNumberOfFunctionsParameters(self):
-        for node in PreOrderIter(self.syntax_tree):
-            if node.name == "CALL_FUNCTION_STMT":
-                isDeclared = self.functionIsDeclared(node)
-                if isDeclared[0]:
-                    numberOfParameters = isDeclared[3]
-                    func_name = isDeclared[2]
-                    numberOfCallParameters = self.getNumberOfCallParameters(node)[0]
-                    line = node.children[0].children[0].line
-                    if numberOfCallParameters != numberOfParameters:
-                        print("Semantic error:\n","In line ", line,", the function '", func_name,
-                        "' expect ", numberOfParameters,", but it receives ", numberOfCallParameters,
-                        sep="")
-                    
-                    
-
-
 
 
 
