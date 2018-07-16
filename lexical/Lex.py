0a0dde525085f0d10180051e2e67d1713cf8f8f1
Paulo Batista <pauloc@linuxmail.org>
2018-07-16 14:46:08 -0300

    new lexical module was added. #65

---
+++ lexical/Lex.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+class lex(object):
+
+    def getToken(self):
+        pass
+    
+
+    def isStopword(self):
+        pass
+    
+    def isSimbol(self):
+        pass
+    
+    def isNumber(self):
+        pass
+        
+    def getTokenlist(self):
+        pass
+    
+    
+    
+
+
